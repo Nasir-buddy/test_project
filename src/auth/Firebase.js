4df0911654c49138955a5f656120816e01517dbe
nasir-buddy <syedali172000@gmail.com>
2025-02-03 01:00:01 +0530

    update some changes

--- src/auth/Firebase.js
+++ src/auth/Firebase.js
@@ -7,17 +7,18 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCvoYv297jML3OzQzRxK3Mzzxv0TuGO4OU",
-  authDomain: "login-auth-83e25.firebaseapp.com",
-  projectId: "login-auth-83e25",
-  storageBucket: "login-auth-83e25.firebasestorage.app",
-  messagingSenderId: "719188200586",
-  appId: "1:719188200586:web:03e5797e7981289ec1b173"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export const auth=getAuth();
-export const db=getFirestore(app);
+export const auth = getAuth();
+export const db = getFirestore(app);
 export default app;
