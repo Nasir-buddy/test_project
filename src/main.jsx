b6b8f5e16b96fa18b15c78ef67bc03299323401a
nasir-buddy <syedali172000@gmail.com>
2025-02-03 11:23:01 +0530

    remove authentication

--- src/main.jsx
+++ src/main.jsx
@@ -12,10 +12,10 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path="/app" element={<App />} />
-        <Route path="/login" element={<Login />}/>
-        <Route path="/" element={<Register />} />
-        <Route path="/register" element={<Register />} />
+        {/* <Route path="/app" element={<App />} /> */}
+        {/* <Route path="/login" element={<Login />}/> */}
+        <Route path="/" element={<App />} />
+        {/* <Route path="/register" element={<Register />} /> */}
         {/* <Route path="/dashboard" element={<App />} /> */}
       </Routes>
     </Router>
