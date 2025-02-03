fac58d4251812944ab8ed8fd815c3f3096b6b917
nasir-buddy <syedali172000@gmail.com>
2025-02-03 11:19:30 +0530

    fix routing

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <Routes>
         <Route path="/app" element={<App />} />
         <Route path="/login" element={<Login />}/>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Register />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<App />} />
       </Routes>
